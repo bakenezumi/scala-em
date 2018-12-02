40a5235fdf0b160c9a668543848d3bacba72258f
bakenezumi <hosonishi@gmail.com>
2018-12-02 22:49:30 +0900

    wip

--- src/main/scala/ml/em/Em.scala
+++ src/main/scala/ml/em/Em.scala
@@ -5,7 +5,7 @@ import breeze.numerics._
 
 object Em extends App {
   implicit class MultivariateGaussianOps(self: MultivariateGaussian) {
-    def generate(n: Int) = DenseMatrix((1 to n).map(_ => self.draw()) : _*)
+    def generate(n: Int) = DenseMatrix((1 to n).map(_ => self.draw()): _*)
   }
   implicit class DenseMatrixOps(self: DenseMatrix[Double]) {
     def matmul(that: DenseMatrix[Double]) =
@@ -14,7 +14,7 @@ object Em extends App {
         for {
           i <- 0 until self.rows
           j <- 0 until that.cols
-        }{
+        } {
           var v = 0.0
           for (k <- 0 until self.cols) v += self(i, k) * that(k, j)
           c(i, j) = v
@@ -25,10 +25,27 @@ object Em extends App {
       }
   }
 
-  val data1 = MultivariateGaussian(mean = DenseVector(1000.0, 80.0), covariance = DenseMatrix((10000.0, 2500.0),(2500.0, 10000.0))).generate(200)
-  val data2 = MultivariateGaussian(mean = DenseVector(200.0, 100.0), covariance = DenseMatrix((12000.0, 1500.0),(1500.0, 12000.0))).generate(100)
-  val data3 = MultivariateGaussian(mean = DenseVector(500.0,-200.0), covariance = DenseMatrix(( 3000.0, -500.0),(-500.0,  2000.0))).generate(100)
-  val data4 = MultivariateGaussian(mean = DenseVector(100.0, 200.0), covariance = DenseMatrix(( 2000.0, 1500.0),(1500.0,  2000.0))).generate(100)
+  val data1 =
+    MultivariateGaussian(mean = DenseVector(1000.0, 80.0),
+                         covariance =
+                           DenseMatrix((10000.0, 2500.0), (2500.0, 10000.0)))
+      .generate(200)
+  val data2 =
+    MultivariateGaussian(mean = DenseVector(200.0, 100.0),
+                         covariance =
+                           DenseMatrix((12000.0, 1500.0), (1500.0, 12000.0)))
+      .generate(100)
+  val data3 =
+    MultivariateGaussian(mean = DenseVector(500.0, -200.0),
+                         covariance =
+                           DenseMatrix((3000.0, -500.0), (-500.0, 2000.0)))
+      .generate(100)
+  val data4 =
+    MultivariateGaussian(mean = DenseVector(100.0, 200.0),
+                         covariance =
+                           DenseMatrix((2000.0, 1500.0), (1500.0, 2000.0)))
+      .generate(100)
+
   val x = DenseMatrix.vertcat(data1, data2, data3, data4)
   println(x)
 
@@ -43,10 +60,46 @@ object Em extends App {
 //  return ( (1 / (np.sqrt(2 * math.pi) ** D * np.sqrt(np.linalg.det(sigma)))) *
 //    np.exp(-0.5 * np.linalg.solve(sigma, (x - mu)) @ (x - mu)).sum() )
 //  return f_
-  def f(mu: DenseMatrix[Double], sigma: DenseMatrix[Double]) = (x: DenseMatrix[Double]) => {
+  def f(mu: DenseMatrix[Double],
+        sigma: DenseMatrix[Double]): DenseMatrix[Double] => Double = x => {
     1 / (pow(sqrt(2 * Math.PI), D) * sqrt(det(sigma))) *
       exp(-0.5 * sum((sigma \ (x - mu)) matmul (x - mu)))
   }
 
+//  def estep(x, mu, sigma, pi):
+//  #pif = np.array([[pk * f(mk, sk)(xn) for xn in x] for pk, mk, sk in zip(pi, mu, sigma)])
+//  pif = np.array([pk * np.apply_along_axis(f(mk, sk), 1, x) for pk, mk, sk in zip(pi, mu, sigma)])
+//  gamma = pif / np.sum(pif, axis=0)
+//  return gamma
+  def estep(x: DenseMatrix[Double],
+            mu: DenseMatrix[Double],
+            sigma: DenseMatrix[Double],
+            pi: DenseMatrix[Double]) = {
+    val pif: DenseMatrix[Double] = zipValues(pi, mu, sigma) { (pk, mk, sk) =>
+      val ff = f(mk, sk)
+      DenseMatrix.vertcat(
+        x(::, *)
+          .foldLeft(List.empty[DenseMatrix[Double]]) { (acc, xn) =>
+            pk * ff(xn.toDenseMatrix) :: acc
+          }
+          .reverse: _*)
+    }
+    pif / sum(pif, Axis._0)
+  }
+
+  val minx = min(x(*, ::))
+  val maxx = max(x(*, ::))
+  val rangex = abs(minx) + abs(maxx)
+  val eqdev = rangex / (K + 1.0)
+
+  val mu = DenseMatrix.vertcat(
+    (0 until K).map(k => (eqdev * (k + 1.0) + minx).toDenseMatrix): _*)
+
+  val sigma = DenseMatrix.vertcat(
+    (0 until K).map(k => (eqdev * (k + 1.0) + minx).toDenseMatrix): _*)
+
+  val pi = DenseVector.ones[Double](K) * (1.0 / K)
+
+  val gamma = estep(x, mu, sigma, pi.toDenseMatrix)
 
 }
